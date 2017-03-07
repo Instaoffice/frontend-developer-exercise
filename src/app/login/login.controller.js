ec2ec85828fcb4f857f0da2ca2454fabc2d2377e
Moid <moid@codemymobile.com>
2017-03-07 14:34:24 +0530

    login feat

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -3,9 +3,38 @@
 
     module.controller('LoginCtrl', LoginCtrl);
 
-    LoginCtrl.$inject = [];
-    function LoginCtrl(){
+    LoginCtrl.$inject = ['$state'];
+    function LoginCtrl($state){
         console.log('login controller');
+
+        var vm = this;
+        vm.valid = {};
+        vm.submitLogin = function submitLogin(){
+            if(vm.username === 'instaoffice-admin' && vm.password === 'admin'){
+                vm.errorMessage = undefined;
+                vm.valid = {
+                    username: 'ng-valid',
+                    password: 'ng-valid'
+                }
+                $state.go('force.list');
+            }
+            else if(vm.username !== 'instaoffice-admin'){
+                vm.errorMessage = 'Username and Password do not match'
+                vm.valid = {
+                    username: 'ng-invalid',
+                    password: 'ng-invalid'
+                }
+            }
+            else{
+                vm.errorMessage = 'Password you have entered does not match the username'
+                vm.valid = {
+                    username: 'ng-valid',
+                    password: 'ng-invalid'
+                }
+            }
+            console.log(vm.errorMessage);
+        }
+
     }
 
 
